9224c586dad5d0de11c8cdace880763954fcca2e
poohbear <452363993@qq.com>
2023-05-02 13:38:33 +0800

    feat: 创建问卷mock服务

---
+++ mock/question.js
@@ -0,0 +1,19 @@
+const Mock = require("mockjs");
+
+const Random = Mock.Random;
+
+module.export = [
+  {
+    url: "/api/question/:id", // 获取单个问卷
+    method: "get",
+    response() {
+      return {
+        errno: 0,
+        data: {
+          id: Random.id(),
+          title: Random.ctitle(),
+        },
+      };
+    },
+  },
+];
