f73ce7d9a2f9c10e44ebb35f309e934e6858665c
poohbear <452363993@qq.com>
2023-06-03 13:31:16 +0800

    feat: 扩展单个组件的统计数据Mock接口

--- mock/stat.js
+++ mock/stat.js
@@ -4,17 +4,37 @@ const getStatList = require('./data/getStatList')
 const Random = Mock.Random
 
 
-// 答卷列表
 module.exports = [
+  // 答卷列表
   {
     url: '/api/stat/:questionId',
+    method: 'get',
+    response(ctx) {
+      const { url = "", query = {} } = ctx;
+      const page = parseInt(query.page) || 1;
+      const pageSize = parseInt(query.pageSize) || 10;
+      return {
+        errno: 0,
+        data: {
+          total: 100,
+          list: getStatList(pageSize)
+        }
+      }
+    }
+  },
+  // 获取单个组件的统计数据汇总
+  {
+    url: '/api/stat/:questionId/:componentId',
     method: 'get',
     response() {
       return {
         errno: 0,
         data: {
-          total: 100,
-          list: getStatList(20)
+          stat: [
+            { name: '选项1', count: 20 },
+            { name: '选项2', count: 10 },
+            { name: '选项3', count: 25 },
+          ]
         }
       }
     }
